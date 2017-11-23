95c1a6ac9f7f0b760d0871186081546aa3bdabec
Pau Sentis <pau.sentis@estudiants.urv.cat>
2017-11-23 16:40:36 +0100

    class Professor

---
+++ src/java/Professor.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Pau
+ */
+public class Professor {
+    
+    private String nom;
+    private String contrasenya;
+    private int id;
+    
+    public Professor(String nom, String contrasenya){
+        this.nom = nom;
+        this.contrasenya = contrasenya;
+    }
+    
+    
+}
