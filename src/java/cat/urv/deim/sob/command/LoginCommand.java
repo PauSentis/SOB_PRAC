b83711a1a3c7290f6cbfa2062351ea83dbb48598
Pau Sentis <pau.sentis@estudiants.urv.cat>
2018-01-10 13:39:51 +0100

    merda varia

--- src/java/cat/urv/deim/sob/command/LoginCommand.java
+++ src/java/cat/urv/deim/sob/command/LoginCommand.java
@@ -6,8 +6,8 @@
 package cat.urv.deim.sob.command;
 
 import cat.urv.deim.sob.Professor;
-import cat.urv.deim.sob.UserDAO;
 import java.io.IOException;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -23,21 +23,25 @@ public class LoginCommand implements Command{
     public void execute(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         
-        Professor profe = new Professor();
+       //Professor profe = new Professor();
         
-       profe.setUsuari(request.getParameter("usuari"));
-       profe.setContrasenya(request.getParameter("pass"));
+       //profe.setUsuari(request.getParameter("usuari"));
+       //profe.setContrasenya(request.getParameter("pass"));
+       ServletContext context = request.getSession().getServletContext();
+       context.getRequestDispatcher("/invalidLogin.jsp").forward(request, response);
+       //profe = UserDAO.login(profe);
        
-       profe = UserDAO.login(profe);
-       
-       if(profe.isValid()){
+        //request.getRequestDispatcher("invalidLogin.jsp").forward(request, response);
+      /* if(profe.isValid()){
            HttpSession session = request.getSession(true);
            session.setAttribute("currentSessionUser", profe);
            response.sendRedirect("userLogged.jsp");
        }
        
        else
-           response.sendRedirect("invalidLogin.jsp");
+           request.getRequestDispatcher("invalidLogin.jsp").forward(request, response);
+           //response.sendRedirect("invalidLogin.jsp");
+*/
 
     }
     
