0ac242c1e4218ae1ba4837c1960f37a23b284236
Toni Ciordia <Toni Ciordia@DESKTOP-KD0AR5O>
2018-01-30 17:57:44 +0100

    29/01

--- SOB_PRAC/src/java/APIRest/RESTProfessor.java
+++ SOB_PRAC/src/java/APIRest/RESTProfessor.java
@@ -5,16 +5,35 @@
  */
 package APIRest;
 
+import Entitats.ProjecteDAO;
+import cat.urv.deim.sob.Projecte;
+import java.io.IOException;
+import java.util.ArrayList;
+import javax.servlet.ServletException;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 /**
  *
  * @author Toni Ciordia
  */
 
-@Path("/rest/api/v1")
+@Path("/professor")
 public class RESTProfessor {
     
-    
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("{id}")
+    public Professor ProfeById(@PathParam("id") String id) throws ServletException, IOException{
+        
+        ProjecteDAO dao = new ProjecteDAO();
+        ArrayList<Projecte> projectes = dao.findByProfessor(Integer.parseInt(id));
+        
+        
+        
+    }
     
 }
