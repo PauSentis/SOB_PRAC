355b903f938591abce983dfb0d03f8bd19f57996
Toni Ciordia <Toni Ciordia@DESKTOP-KD0AR5O>
2018-01-30 17:42:12 +0100

    29/01

---
+++ SOB_PRAC/src/java/APIRest/ApplicationConfig.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package APIRest;
+
+import java.util.HashSet;
+import java.util.Set;
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ * @author Toni Ciordia
+ */
+@ApplicationPath("/rest/api/v1")
+public class ApplicationConfig extends Application{
+    
+    @Override
+    public Set<Class<?>> getClasses(){
+        
+        final Set<Class<?>> classes = new HashSet<>();
+        
+        return classes;
+    }
+    
+}
---
+++ SOB_PRAC/src/java/APIRest/RESTProjecte.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package APIRest;
+
+import javax.ws.rs.Path;
+
+/**
+ *
+ * @author Toni Ciordia
+ */
+
+@Path("/rest/api/v1")
+public class RESTProjecte {
+    
+    
+    
+}
